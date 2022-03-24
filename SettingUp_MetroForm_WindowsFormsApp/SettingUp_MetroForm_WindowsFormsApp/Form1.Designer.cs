ef818e3e3bbe62848d4aa341358baa18734da285
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-24 16:33:50 +0600

    Setting up metro design part modified

--- SettingUp_MetroForm_WindowsFormsApp/SettingUp_MetroForm_WindowsFormsApp/Form1.Designer.cs
+++ SettingUp_MetroForm_WindowsFormsApp/SettingUp_MetroForm_WindowsFormsApp/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackImage = ((System.Drawing.Image)(resources.GetObject("$this.BackImage")));
+            this.BackMaxSize = 923;
             this.ClientSize = new System.Drawing.Size(923, 649);
             this.Name = "Form1";
             this.Resizable = false;
