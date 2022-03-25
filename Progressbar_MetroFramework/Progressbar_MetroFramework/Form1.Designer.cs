4689d601a2a6b63cbfc9b76de7a484de7d9dcb81
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-25 12:02:52 +0600

    Progressbar in metro framework

---
+++ Progressbar_MetroFramework/Progressbar_MetroFramework/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Progressbar_MetroFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroProgressBar = new MetroFramework.Controls.MetroProgressBar();
+            this.btnStart = new MetroFramework.Controls.MetroButton();
+            this.btnStop = new MetroFramework.Controls.MetroButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroLabel = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroProgressBar
+            // 
+            this.metroProgressBar.HideProgressText = false;
+            this.metroProgressBar.Location = new System.Drawing.Point(121, 111);
+            this.metroProgressBar.Name = "metroProgressBar";
+            this.metroProgressBar.Size = new System.Drawing.Size(511, 60);
+            this.metroProgressBar.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.btnStart.Location = new System.Drawing.Point(128, 214);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(90, 45);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseSelectable = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.btnStop.Location = new System.Drawing.Point(307, 214);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(90, 45);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseSelectable = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // metroLabel
+            // 
+            this.metroLabel.AutoSize = true;
+            this.metroLabel.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel.Location = new System.Drawing.Point(465, 219);
+            this.metroLabel.Name = "metroLabel";
+            this.metroLabel.Size = new System.Drawing.Size(118, 25);
+            this.metroLabel.TabIndex = 2;
+            this.metroLabel.Text = "metroLabel1";
+            this.metroLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 581);
+            this.Controls.Add(this.metroLabel);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.metroProgressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar;
+        private MetroFramework.Controls.MetroButton btnStart;
+        private MetroFramework.Controls.MetroButton btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroLabel metroLabel;
+    }
+}
+
---
+++ Progressbar_MetroFramework/Progressbar_MetroFramework/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace Progressbar_MetroFramework
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            metroProgressBar.Increment(1);
+            int value = metroProgressBar.Value;
+
+            metroLabel.Text = "Progressing..." + value + "%";
+            metroLabel.Visible = true;
+            
+            if(metroProgressBar.Value == 100)
+            {
+                timer1.Stop();
+                metroLabel.Text = "Done";
+                Welcome welcome = new Welcome();
+
+                this.Hide();
+                welcome.Show();
+                
+
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+          
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
